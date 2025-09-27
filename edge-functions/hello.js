086288985a004dd8f1a792c8d770919f615deaf9
stackmastery <labib.developers@gmail.com>
2025-09-27 20:33:57 +0600

    first commit

--- edge-functions/hello.js
+++ edge-functions/hello.js
@@ -1,3 +1,2 @@
-// netlify/edge-functions/hello.ts
-export default () => new Response("Hello from Netlify Edge!");
-export const config = { path: "/test" };
+export default () => new Response("Hello from Edge with JS!");
+export const config = { path: "/" };
